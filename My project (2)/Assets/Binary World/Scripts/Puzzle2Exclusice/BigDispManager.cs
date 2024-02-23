a4c4b2953367c6d1f248d433100c983a34357d03
gurramoller <74371542+gurramoller@users.noreply.github.com>
2024-02-23 09:37:57 +0100

    Update BigDispManager.cs

--- My project (2)/Assets/Binary World/Scripts/Puzzle2Exclusice/BigDispManager.cs
+++ My project (2)/Assets/Binary World/Scripts/Puzzle2Exclusice/BigDispManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,26 +6,53 @@ using UnityEngine;
 public class BigDispManager : MonoBehaviour
 {
     private LongBoxSpawner[] _longBoxSpawners;
-    private int boxesCollided;
+    private GameObject[] _assemblyLines;
+    private ButtonManagerScript _buttonParent;
+    private GameObject[] _boxes;
+
     private int val;
+    private int index;
     private bool tryValue;
 
     void Start()
     {
         _longBoxSpawners = FindObjectsOfType<LongBoxSpawner>();
-        boxesCollided = 0;
+        _buttonParent = FindObjectOfType<ButtonManagerScript>();
+        AddAssemblyLines();
+
+
+        UpdateAssembly(true);
+
         tryValue = false;
+        index = 0;
+
+    }
+
+    private void Update()
+    {
+        if (!tryValue) return;
+
+        if (_buttonParent.CompareValue(val)) OnSuccess();
+        
+    }
+
+
+    private void AddAssemblyLines()
+    {
+        _assemblyLines = GameObject.FindGameObjectsWithTag("Assembly");
     }
 
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        boxesCollided++;
         val += collision.gameObject.GetComponent<BoxScript>().GetValue();
-        if (boxesCollided == 2)
+        _boxes[index] = collision.gameObject;
+        index++;
+        if (index == 2)
         {
             StopBoxes();
             tryValue = true;
+            UpdateAssembly(false);
             Debug.Log(val);
         }
     }
@@ -40,4 +68,30 @@ public class BigDispManager : MonoBehaviour
         _longBoxSpawners[0].StartBoxes();
         _longBoxSpawners[1].StartBoxes();
     }
+
+    private void UpdateAssembly(bool on)
+    {
+        foreach (GameObject line in _assemblyLines)
+        {
+            line.GetComponent<Animator>().SetBool("isOn", on);
+        }
+    }
+
+    private void OnSuccess()
+    {
+        val = 0;
+        tryValue = false;
+        index = 0;
+        
+        UpdateAssembly(true);
+    }
+
+    private void DestroyBoxes()
+    {
+        foreach(GameObject box in _boxes)
+        {
+            Destroy(box);
+        }
+    }
+
 }
