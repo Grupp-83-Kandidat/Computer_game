c67979336a69fe09bb7631e86f2659a4bba7b017
Johannes Bexell <bexellj@student.chalmers.se>
2024-03-05 19:25:50 +0100

    Continued work on hexpuzzle 1

--- ComputerGame2/Assets/Hex World/Scripts/BinaryEightDisplayScript.cs
+++ ComputerGame2/Assets/Hex World/Scripts/BinaryEightDisplayScript.cs
@@ -1,40 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class BinaryEightDisplayScript : MonoBehaviour
 {
-    private SpriteRenderer _spriteRenderer;
+    public DictEntryDbit[] bits = new DictEntryDbit[8];
+    //private SpriteRenderer _spriteRenderer;
 
     private int _value;
 
-    private Dictionary<int, Sprite> spriteDict = new Dictionary<int, Sprite>();
+    //private Dictionary<int, Sprite> spriteDict = new Dictionary<int, Sprite>();
 
-    private BoxSpawnerScript _boxSpawner;
+    //private BoxSpawnerScript _boxSpawner;
 
-    private bool _tryValue = false;
+    //private bool _tryValue = false;
 
-    private GameObject[] AssemblyLines;
+    //private GameObject[] AssemblyLines;
 
-    private int _score = 0;
+    //private int _score = 0;
 
-    public Sprite[] sprites = new Sprite[1];
+    //public Sprite[] sprites = new Sprite[1];
 
 
     // Start is called before the first frame update
     void Start()
     {
-        Init(); 
+        //Init(); 
+        UpdateBits(16);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Attempt(); 
+        //Attempt(); 
     }
+    /*
 
-
-private void Init()
+    private void Init()
     {
         AddSprites();
         AddAssemblyLines();
@@ -77,7 +81,7 @@ private void Init()
 
     private void UpdateAssembly(bool on)
     {
-        foreach(GameObject line in AssemblyLines)
+        foreach (GameObject line in AssemblyLines)
         {
             line.GetComponent<Animator>().SetBool("isOn", on);
         }
@@ -98,12 +102,39 @@ private void Init()
     }
 
 
-    void OnCollisionEnter2D(Collision2D col){
-        BoxScript box = (BoxScript) col.gameObject.GetComponent(typeof(BoxScript)); 
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        BoxScript box = (BoxScript)col.gameObject.GetComponent(typeof(BoxScript));
         UpdateDisplay(box.GetValue());
         Destroy(col.gameObject);
         UpdateAssembly(false);
         _boxSpawner.StopBoxes();
         _tryValue = true;
+    }*/
+    private void UpdateBits(int val) {
+        int[] binaryArray = DecimalToBinaryArray(val);
+        for (int i = 0; i < 8; i++)
+        {
+            bits[i].bitObject.ChangeNumber(binaryArray[7-i]);
+        }
     }
+    static int[] DecimalToBinaryArray(int number)
+    {
+        int[] binaryArray = new int[8];
+        int index = 7;
+        while (number > 0 && index >= 0)
+        {
+            binaryArray[index--] = (int)(number % 2);
+            number /= 2;
+        }
+
+        return binaryArray;
+    }
+}
+
+[Serializable]
+public struct DictEntryDbit {
+    public int bitNo;
+    public DisplayBitScript bitObject;
+
 }
--- ComputerGame2/Assets/Hex World/Scripts/DisplayBitScript.cs
+++ ComputerGame2/Assets/Hex World/Scripts/DisplayBitScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class DisplayBitScript : MonoBehaviour
 {
+    public Sprite[] sprites = new Sprite[2];
+    private SpriteRenderer _spriteRenderer;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,18 @@ public class DisplayBitScript : MonoBehaviour
     void Update()
     {
         
+    }
+    private void SetSprite(Sprite sprite){
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRenderer.sprite = sprite;
+    }
+
+    public void ChangeNumber(int oneOrZero) {
+        if (oneOrZero == 1) {
+            SetSprite(sprites[1]);
+        }
+        else {
+            SetSprite(sprites[0]);
+        }
     }
 }
