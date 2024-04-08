a1741136d9b7dbc2e8175e2f7d8c7e5964252e41
Christoffer0108 <christofferhenriksson0108@gmail.com>
2024-04-09 01:34:26 +0200

    Clock

--- ComputerGame2/Assets/Gate World/Scripts/PuzzleScripts/Functionality/Clocka.cs
+++ ComputerGame2/Assets/Gate World/Scripts/PuzzleScripts/Functionality/Clocka.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
 
 public class Clocka : ElectricityControll
 {
@@ -9,30 +11,30 @@ public class Clocka : ElectricityControll
     [SerializeField] private List<Sprite> sprites;
     private List<Collider2D> inputList = new(1);
     public List<Collider2D> inputs = new(4);
-    Dictionary<int, List<bool>> binaryNum;
+    Dictionary<List<bool>, int> binaryNum;
 
     void Start(){
         for (int i = 0; i < transform.childCount; i++){
             inputs.Add(transform.GetChild(i).GetComponent<Collider2D>());
         }
 
-        binaryNum = new Dictionary<int, List<bool>>() {
-            {0 , new List<bool> {false, false, false, false}},
-            {1 , new List<bool> {false, false, false, true}},
-            {2 , new List<bool> {false, false, true, false}},
-            {3 , new List<bool> {false, false, true, true}},
-            {4 , new List<bool> {false, true, false, false}},
-            {5 , new List<bool> {false, true, false, true}},
-            {6 , new List<bool> {false, true, true, false}},
-            {7 , new List<bool> {false, true, true, true}},
-            {8 , new List<bool> {true, false, false, false}},
-            {9 , new List<bool> {true, false, false, true}},
-            {10 , new List<bool> {true, false, true, false}},
-            {11 , new List<bool> {true, false, true, true}},
-            {12 , new List<bool> {true, true, false, false}},
-            {13 , new List<bool> {true, true, false, true}},
-            {14 , new List<bool> {true, true, true, false}},
-            {15 , new List<bool> {true, true, true, true}}
+        binaryNum = new Dictionary<List<bool>, int>() {
+            {new List<bool> {false, false, false, false}, 0},
+            {new List<bool> {false, false, false, true}, 1},
+            {new List<bool> {false, false, true, false}, 2},
+            {new List<bool> {false, false, true, true}, 3},
+            {new List<bool> {false, true, false, false}, 4},
+            {new List<bool> {false, true, false, true}, 5},
+            {new List<bool> {false, true, true, false}, 6},
+            {new List<bool> {false, true, true, true}, 7},
+            {new List<bool> {true, false, false, false}, 8},
+            {new List<bool> {true, false, false, true}, 9},
+            {new List<bool> {true, false, true, false}, 10},
+            {new List<bool> {true, false, true, true}, 11},
+            {new List<bool> {true, true, false, false}, 12},
+            {new List<bool> {true, true, false, true}, 13},
+            {new List<bool> {true, true, true, false}, 14},
+            {new List<bool> {true, true, true, true}, 15}
         };
     }
 
@@ -46,7 +48,12 @@ public class Clocka : ElectricityControll
     }
 
     private void DisplayNumber(List<bool> conductingList){
-        GetComponent<UnityEngine.UI.Image>().sprite = litLamp;
+        foreach(var item in binaryNum){
+            
+            if(item.Key.SequenceEqual(conductingList)){
+                GetComponent<UnityEngine.UI.Image>().sprite = sprites[item.Value];
+                Debug.Log(sprites[item.Value]);
+            }
+        }
     }
-
 }
