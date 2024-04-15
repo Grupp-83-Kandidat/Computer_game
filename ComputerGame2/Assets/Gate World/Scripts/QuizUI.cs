7721d1c38a8c216d1eae3710513ebf20e35e5560
alexolssson <alexander.00.olsson@gmail.com>
2024-04-16 00:16:10 +0200

    QuizUI and QuizManager

--- ComputerGame2/Assets/Gate World/Scripts/QuizUI.cs
+++ ComputerGame2/Assets/Gate World/Scripts/QuizUI.cs
@@ -2,28 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class QuizUI : MonoBehaviour
 {
     [SerializeField] private QuizManager quizManager;
-    [SerializeField] private Text questionText;
+    [SerializeField] private Text questionText, scoreText, timerText;
+    [SerializeField] private List<Image> lifeImageList;
+    [SerializeField] private GameObject gameOverPanel, mainMenuPanel, gameMenuPanel;
     [SerializeField] private Image questionImage;
     [SerializeField] private UnityEngine.Video.VideoPlayer questionVideo;
     [SerializeField] private AudioSource questionAudio;
-    [SerializeField] private List<Button> options;
+    [SerializeField] private List<Button> options, uiButtons;
     [SerializeField] private Color correctCol, wrongCol, normalCol;
 
     private Question question;
     private bool answered;
     private float audioLength;
+
+    public Text ScoreText { get { return scoreText; } }
+
+    public Text TimerText { get { return timerText; } }
+
+    public GameObject GameOverPanel { get { return gameOverPanel; } }
     
     // Start is called before the first frame update
-    void Awake()
+    void Start()
     {
         for(int i = 0; i < options.Count; i++){
             Button localBtn = options[i];
             localBtn.onClick.AddListener(() => OnClick(localBtn));
-        }   
+        }  
+
+        for(int i = 0; i < uiButtons.Count; i++){
+            Button localBtn = uiButtons[i];
+            localBtn.onClick.AddListener(() => OnClick(localBtn));
+        } 
     }
 
     public void SetQuestion(Question question){
@@ -54,7 +68,8 @@ public class QuizUI : MonoBehaviour
 
         questionText.text = question.questionInfo;
 
-        List<string> answerList = ShuffleList.ShuffleListItems<string>(question.options);
+        //List<string> answerList = ShuffleList.ShuffleListItems<string>(question.options);
+        List<string> answerList = question.options;
 
         for(int i = 0; i < options.Count; i++){
             options[i].GetComponentInChildren<Text>().text = answerList[i];
@@ -84,7 +99,9 @@ public class QuizUI : MonoBehaviour
 
     }
 
-    void OnClick(Button btn){
+    private void OnClick(Button btn){
+        if (quizManager.GameStatus == GameStatus.Playing){
+
         if(!answered){
             answered = true;
             bool val = quizManager.Answer(btn.name);
@@ -97,8 +114,34 @@ public class QuizUI : MonoBehaviour
             }
         }
     }
+    switch (btn.name){
+        case "NAND":
+            quizManager.StartGame(0);
+            mainMenuPanel.SetActive(false);
+            gameMenuPanel.SetActive(true);
+            break;
+        case "OR":
+            quizManager.StartGame(1);
+            mainMenuPanel.SetActive(false);
+            gameMenuPanel.SetActive(true);
+            break;
+        case "Mix":
+            quizManager.StartGame(2);
+            mainMenuPanel.SetActive(false);
+            gameMenuPanel.SetActive(true);
+            break;        
+    }
+    }
 
-    // Update is called once per frame
+    /*public void RetryButton(){
+        SceneManagerGates.LoadScene()
+    }*/
+
+    public void ReduceLife(int index){
+        lifeImageList[index].color = wrongCol;
+    }
+
+    // Update is called once per frame (currently not used).
     void Update()
     {
         
